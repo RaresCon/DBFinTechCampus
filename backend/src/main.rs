5c77fd6f79b9d40bb291513ece91ae38b6c120bb
RaresCon <rares.constantin2002@gmail.com>
2023-04-08 15:19:33 +0300

    CORS should work
    Signed-off-by: RaresCon <rares.constantin2002@gmail.com>

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,35 +5,25 @@ mod repository;
 // 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
 #[macro_use] extern crate rocket;
 use rocket::{get, http::Status, Request, Response, serde::json::Json};
-use rocket::fairing::{Fairing, Info, Kind};
+use rocket::fairing::{AdHoc, Fairing, Info, Kind};
 use rocket::http::{Header, Method};
-use rocket_cors::AllowedHeaders;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
 use crate::repository::repository::MongoRepo;
 use api::user_api::{create_user, get_user};
 
 #[launch]
 fn rocket() -> _ {
     let db = MongoRepo::init();
+    let cors = CorsOptions::default().allowed_origins(AllowedOrigins::all())
+                                     .allowed_methods(
+                                        vec![Method::Get, Method::Post, Method::Patch]
+                                            .into_iter()
+                                            .map(From::from)
+                                            .collect(),
+                                     );
+
     rocket::build().manage(db)
-                   .attach(CORS)
+                   .attach(cors.to_cors().unwrap())
                    .mount("/", routes![create_user])
-}
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
+                   .mount("/", routes![get_user])
 }

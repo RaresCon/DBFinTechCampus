f47b3aa568e45dbe02366fe5a8f73a172ccffbc1
Robert Grancsa <robert.grancsa2002@gmail.com>
2023-04-08 13:20:52 +0300

    Added more features
    Signed-off-by: Robert Grancsa <robert.grancsa2002@gmail.com>

--- front-end/db_frontend/src/components/login.component.jsx
+++ front-end/db_frontend/src/components/login.component.jsx
@@ -1,19 +1,51 @@
 import {Link} from "react-router-dom";
 import {useForm} from "react-hook-form";
+import axios from "axios";
+import {useCookies} from "react-cookie";
+import {useState} from "react";
 
 
 export default function LoginComponent() {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const [cookies, setCookie, removeCookie] = useCookies(['token']);
+    const [notification, setNotification] = useState({});
+
+    const onSubmit = data => {
+        console.log(data);
+
+        let log = {
+            e_mail: data.e_mail,
+            password: data.password
+        }
+
+        axios.post("enter-link", log)
+            .then(res => {
+                console.log(res.data);
+                setCookie("token", {
+                    token: res.data.token,
+                    email: data.e_mail
+                }, {
+                    path: "/"
+                })
+                window.location.replace("/home");
+            })
+            .catch((error) => {
+                console.log(error);
+                setNotification({
+                    message: error.response.data.statusMessage.substring(8),
+                    isError: true
+                })
+            });
+    };
 
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <img className="w-8 h-8 mr-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Deutsche_Bank_logo_without_wordmark.svg/1200px-Deutsche_Bank_logo_without_wordmark.svg.png"
                          alt="logo"/>
-                        Flowbite
-                </a>
+                        DB Student
+                </Link>
                 <div
                     className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -27,15 +59,14 @@ export default function LoginComponent() {
                                     email</label>
                                 <input type="email" name="email" id="email"
                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       placeholder="name@company.com" required=""/>
+                                       placeholder="name@company.com" required="" {...register("e_mail", {required: true, pattern: /^\S+@\S+$/i})}/>
                             </div>
-                            {errors.exampleRequired && <span>This field is required</span>}
                             <div>
                                 <label htmlFor="password"
                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                                 <input type="password" name="password" id="password" placeholder="••••••••"
                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       required=""/>
+                                       required="" {...register("password", {required: true})}/>
                             </div>
                             <div className="flex items-center justify-between">
                                 <div className="flex items-start">
@@ -57,6 +88,8 @@ export default function LoginComponent() {
                                     className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
                                 in
                             </button>
+                            {notification.isError && <span className="font-light text-red-500 dark:text-red-300">{notification.message}</span>}
+
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <Link to="/register"
                                                               className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign

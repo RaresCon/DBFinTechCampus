4acabf8092c63aebd4b53d5b53461ffbe166f81e
RaresCon <rares.constantin2002@gmail.com>
2023-04-08 13:56:07 +0300

    fixed other problems
    Signed-off-by: RaresCon <rares.constantin2002@gmail.com>

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -13,13 +13,17 @@ use api::user_api::{create_user, get_user};
 #[launch]
 fn rocket() -> _ {
     let db = MongoRepo::init();
-    rocket::build().attach(Cors).manage(db).mount("/", routes![create_user])
+    rocket::build().attach(CorsResponse)
+                   .attach(CorsRequest)
+                   .manage(db)
+                   .mount("/", routes![create_user])
 }
 
-pub struct Cors;
+pub struct CorsResponse;
+pub struct CorsRequest;
 
 #[rocket::async_trait]
-impl Fairing for Cors {
+impl Fairing for CorsResponse {
     fn info(&self) -> Info {
         Info {
             name: "Cross-Origin-Resource-Sharing Fairing",
@@ -28,6 +32,26 @@ impl Fairing for Cors {
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+#[rocket::async_trait]
+impl Fairing for CorsRequest {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Request,
+        }
+    }
+
+    async fn on_request<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
         response.set_header(Header::new(
             "Access-Control-Allow-Methods",

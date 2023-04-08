af700b5da4c18d9936953d6b7a6a2baad801c64e
RaresCon <rares.constantin2002@gmail.com>
2023-04-08 13:51:41 +0300

    fixed problems
    Signed-off-by: RaresCon <rares.constantin2002@gmail.com>

--- backend/src/api/user_api.rs
+++ backend/src/api/user_api.rs
@@ -3,13 +3,13 @@ use mongodb::results::InsertOneResult;
 use rocket::{http::Status, serde::json::Json, State};
 use sha2::{Sha256, Digest, digest};
 use sha2::digest::FixedOutput;
-use crate::models::user_model::UserLogin;
+use crate::models::user_model::{BearerToken, UserLogin};
 
 #[post("/user", data = "<new_user>")]
 pub fn create_user(
     db: &State<MongoRepo>,
     new_user: Json<User>,
-) -> Result<Json<String>, Status> {
+) -> Result<Json<BearerToken>, Status> {
     let pass_hash = format!("{}", create_hash(new_user.password.clone().as_str(), Sha256::default()));
 
     let user = User {
@@ -20,9 +20,10 @@ pub fn create_user(
     };
     let user_detail = db.create_user(user);
 
-    let e_mail_hash = format!("{}", create_hash(new_user.e_mail.clone().as_str(), Sha256::default()));
+    let token = BearerToken { token: format!("{}", create_hash(new_user.e_mail.clone().as_str(),
+                                                               Sha256::default())) };
     match user_detail {
-        Ok(_user) => Ok(Json(e_mail_hash)),
+        Ok(_user) => Ok(Json(token)),
         Err(_) => Err(Status::InternalServerError),
     }
 }

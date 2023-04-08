2e6f254b8f6d8a1dcf98f9022274510500525218
Robert Grancsa <robert.grancsa2002@gmail.com>
2023-04-08 12:59:10 +0300

    Finished login and register
    Signed-off-by: Robert Grancsa <robert.grancsa2002@gmail.com>

---
+++ front-end/db_frontend/src/components/register.component.jsx
@@ -0,0 +1,113 @@
+import {Link} from "react-router-dom";
+import {useForm} from "react-hook-form";
+import axios from "axios";
+import {useCookies} from "react-cookie";
+
+
+export default function RegisterComponent() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
+
+    const onSubmit = data => {
+        const reg = {
+            e_mail: data.e_mail,
+            password: data.password,
+            name: data.name,
+        }
+
+        axios.post("enter-link", reg)
+            .then(res => {
+                console.log(res.data);
+                setCookie("userToken", {
+                    token: res.data.token,
+                    email: data.e_mail
+                }, {
+                    path: "/"
+                });
+                setNotification({
+                    message: res.data,
+                    isError: false,
+                }))
+            .catch();
+
+        console.log(reg)
+    };
+
+    return (
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                         alt="logo"/>
+                        DB Student
+                </a>
+                <div
+                    className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Create and account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6"  onSubmit={handleSubmit(onSubmit)}>
+                            <div>
+                                <label htmlFor="name"
+                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your full name</label>
+                                <input type="name" name="name" id="name"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="John Doe" required="" {...register("name")}/>
+                            </div>
+
+                            <div>
+                                <label htmlFor="email"
+                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                    email</label>
+                                <input type="email" name="email" id="email"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="name@company.com" required="" {...register("e_mail", {required: true, pattern: /^\S+@\S+$/i})}/>
+                            </div>
+                            <div>
+                                <label htmlFor="password"
+                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       required=""/>
+                            </div>
+                            <div>
+                                <label htmlFor="confirm-password"
+                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
+                                    password</label>
+                                <input type="confirm-password" name="confirm-password" id="confirm-password"
+                                       placeholder="••••••••"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       required="" {...register("password", {required: true})}/>
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="terms" aria-describedby="terms" type="checkbox"
+                                           className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                           required="" {...register("terms", {required: true})}/>
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I
+                                        accept the <a
+                                            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                            href="#">Terms and Conditions</a></label>
+                                </div>
+
+                            </div>
+                            {errors.terms && <span className="font-light text-red-500 dark:text-red-300">This field is required</span>}
+                            <button type="submit"
+                                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create
+                                an account
+                            </button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Already have an account? <Link to="/login"
+                                                            className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login
+                                here</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
